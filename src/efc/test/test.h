0082c1c726ac81f0a5cabb4808225ce061551522
sensorflo <sensorflo@gmail.com>
2014-05-06 15:36:25 +0200

    commit fix for 'efc/test: added ENV_ASSERT family of macros'

--- src/efc/test/test.h
+++ src/efc/test/test.h
@@ -16,10 +16,15 @@
 // difference is that the semantic of ASSERT is to be in the verify section of
 // the test, i.e. its about wether the test succeeded or failed. When an
 // ENV_ASSERT fails, then it is unknown whether the test succeeded or failed,
-// the test was aborted.
-#define ENV_ASSERT_EQ(expected, actual) ASSERT_EQ(expected, actual)
-#define ENV_ASSERT_NE(expected, actual) ASSERT_NE(expected, actual)
-#define ENV_ASSERT_TRUE(actual) ASSERT_TRUE(actual)
-#define ENV_ASSERT_FALSE(actual) ASSERT_FALSE(actual)
+// the test was aborted. When a test fails, than the defect is most likely in
+// what was exercised of the unit under test. When a test aborts, the defect
+// is somewhere else, and given you have a unit test for everything, there
+// should be at least one other test which fails (not aborts), and that
+// failing test will lead you to the cause of the defect.
+#define ENV_ASSERT_EQ(expected, actual) ASSERT_EQ(expected, actual) << ABORT_MSG
+#define ENV_ASSERT_NE(expected, actual) ASSERT_NE(expected, actual) << ABORT_MSG
+#define ENV_ASSERT_TRUE(actual) ASSERT_TRUE(actual) << ABORT_MSG
+#define ENV_ASSERT_FALSE(actual) ASSERT_FALSE(actual) << ABORT_MSG
+#define ABORT_MSG "test aborted due to failing assertion in test's environment\\n"
 
 #endif
