4cc60632b71edcbed3cd1fa7846619e53a347c5d
sensorflo <sensorflo@gmail.com>
2014-05-08 10:10:34 +0200

    irbuilderasttest: refactored

--- src/efc/test/irbuilderasttest.cpp
+++ src/efc/test/irbuilderasttest.cpp
@@ -4,60 +4,35 @@
 using namespace testing;
 using namespace std;
 
+void testbuilAndRunModule(AstSeq* astSeq, int expectedResult) {
+  // setup
+  ENV_ASSERT_TRUE( astSeq!=NULL );
+  auto_ptr<AstSeq> astSeqAp(astSeq);
+  IrBuilderAst UUT;
+
+  // execute & verify
+  EXPECT_EQ(expectedResult, UUT.buildAndRunModule(*astSeq));
+}
+
 TEST(IrBuilderAstTest, MAKE_TEST_NAME(
     a_seq_with_one_or_more_numbers,
     buildAndRunModule,
     returns_the_last_number)) {
-  // example 1
-  {
-    // setup
-    IrBuilderAst UUT;
-    auto_ptr<AstSeq> astSeq(new AstSeq(new AstNumber(42)));
-    ENV_ASSERT_TRUE( astSeq.get()!=NULL );
-
-    // execute & verify
-    EXPECT_EQ(42, UUT.buildAndRunModule(*astSeq.get()));
-  }
-
-  // example 2
-  {
-    // setup
-    IrBuilderAst UUT;
-    auto_ptr<AstSeq> astSeq(new AstSeq(new AstNumber(11), new AstNumber(22)));
-    ENV_ASSERT_TRUE( astSeq.get()!=NULL );
-
-    // execute & verify
-    EXPECT_EQ(22, UUT.buildAndRunModule(*astSeq));
-  }
+  testbuilAndRunModule(new AstSeq(new AstNumber(42)), 42);
+  testbuilAndRunModule(new AstSeq(new AstNumber(11), new AstNumber(22)), 22);
 }
 
 TEST(IrBuilderAstTest, MAKE_TEST_NAME(
     a_seq_with_one_or_more_expressions_built_with_literal_numbers,
     buildAndRunModule,
     returns_the_result_of_the_last_expression)) {
-
-  // example 1
-  {
-    // setup
-    IrBuilderAst UUT;
-    auto_ptr<AstSeq> astSeq(new AstSeq(new AstOperator('+', new AstNumber(2), new AstNumber(3))));
-    ENV_ASSERT_TRUE( astSeq.get()!=NULL );
-
-    // execute & verify
-    EXPECT_EQ(2+3, UUT.buildAndRunModule(*astSeq.get()));
-  }
-
-  // example 2
-  {
-    // setup
-    IrBuilderAst UUT;
-    auto_ptr<AstSeq> astSeq(
-      new AstSeq(
-        new AstOperator('+', new AstNumber(2), new AstNumber(3)),
-        new AstOperator('*', new AstNumber(4), new AstNumber(5))));
-    ENV_ASSERT_TRUE( astSeq.get()!=NULL );
-
-    // execute & verify
-    EXPECT_EQ( (/*2+3,*/4*5), UUT.buildAndRunModule(*astSeq.get()));
-  }
+  testbuilAndRunModule(
+    new AstSeq(
+      new AstOperator('+', new AstNumber(2), new AstNumber(3))),
+    2+3);
+  testbuilAndRunModule(
+    new AstSeq(
+      new AstOperator('+', new AstNumber(2), new AstNumber(3)),
+      new AstOperator('*', new AstNumber(4), new AstNumber(5))),
+    /*2+3,*/ 4*5);
 }
