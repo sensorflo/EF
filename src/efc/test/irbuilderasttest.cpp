85b2d7be2a6b9aa66576fd7f8db8bc2b9e2992f4
sensorflo <sensorflo@gmail.com>
2014-05-13 22:07:50 +0200

    commit fix for 'learned to produce IR for function call'

--- src/efc/test/irbuilderasttest.cpp
+++ src/efc/test/irbuilderasttest.cpp
@@ -311,6 +311,19 @@ TEST(IrBuilderAstTest, MAKE_TEST_NAME(
 }
 
 TEST(IrBuilderAstTest, MAKE_TEST_NAME(
+    a_function_call_to_an_defined_function,
+    buildAndRunModule,
+    returns_result_of_that_function_call)) {
+  TEST_BUILD_AND_RUN_MODULE(
+    new AstSeq(
+      new AstFunDef(
+        new AstFunDecl("foo"),
+        new AstSeq(new AstNumber(42))),
+      new AstFunCall("foo")),
+    42, "");
+}
+
+TEST(IrBuilderAstTest, MAKE_TEST_NAME(
     a_data_definition_of_foo_being_initialized_with_x,
     buildAndRunModule,
     returns_x)) {
