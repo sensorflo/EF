9df5209476cda51932e23e5d8c2a0aff0ed33f1b
sensorflo <sensorflo@gmail.com>
2014-05-13 21:52:36 +0200

    main now compiles the file given as command line argument and JIT executes it

--- src/efc/efc.cpp
+++ src/efc/efc.cpp
@@ -1,6 +1,21 @@
 #include "irbuilderast.h"
+#include "driver.h"
+#include "ast.h"
+#include <iostream>
+using namespace std;
 
-int main(int, char**) {
+int main(int argc, char** argv) {
+  if (argc!=2) {
+    cerr << "Only exactly one argument, the EF program, is allowed.";
+    exit(1);
+  }
   IrBuilderAst::staticOneTimeInit();
+  Driver driver(argv[1]);
+  AstSeq* ast = NULL;
+  if (driver.parse(ast)) {
+    exit(1);
+  }
+  IrBuilderAst irBuilderAst;
+  cout << "Result = " << irBuilderAst.buildAndRunModule(*ast) << "\n";
   return 0;
 }
