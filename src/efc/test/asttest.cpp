7aaed72de032987df9b6cda9543f53a48e9ff593
sensorflo <sensorflo@gmail.com>
2014-05-14 08:16:13 +0200

    commit fix for 'tests: improved quality of output'

--- src/efc/test/asttest.cpp
+++ src/efc/test/asttest.cpp
@@ -3,62 +3,65 @@
 using namespace std;
 using namespace testing;
 
-TEST(AstTest, MAKE_TEST_NAME1(toStr)) {
-  // AstNumber
-  EXPECT_EQ( "42", AstNumber(42).toStr() );
+TEST(AstTest, MAKE_TEST_NAME2(
+    toStr,
+    returns_the_canonical_string_representation_of_the_AST)) {
 
-  // AstSymbol
-  EXPECT_EQ( "foo", AstSymbol(new string("foo")).toStr() );
+  string spec = "AstNumber";
+  EXPECT_EQ( "42", AstNumber(42).toStr() ) << amendSpec(spec);
 
-  // AstSeq
-  EXPECT_EQ( "seq()", AstSeq().toStr() );
-  EXPECT_EQ( "seq(42)", AstSeq(new AstNumber(42)).toStr() );
-  EXPECT_EQ( "seq(42 77)", AstSeq(new AstNumber(42), new AstNumber(77)).toStr() );
+  spec = "AstSymbol";
+  EXPECT_EQ( "foo", AstSymbol(new string("foo")).toStr()) << amendSpec(spec);
 
-  // AstOperator
+  spec = "AstSeq";
+  EXPECT_EQ( "seq()", AstSeq().toStr() ) << amendSpec(spec);
+  EXPECT_EQ( "seq(42)", AstSeq(new AstNumber(42)).toStr() ) << amendSpec(spec);
+  EXPECT_EQ( "seq(42 77)", AstSeq(new AstNumber(42), new AstNumber(77)).toStr() ) << amendSpec(spec);
+
+  spec = "AstOperator";
   EXPECT_EQ( "+(42 77)",
-    AstOperator('+',new AstNumber(42), new AstNumber(77)).toStr() );
+    AstOperator('+',new AstNumber(42), new AstNumber(77)).toStr() ) << amendSpec(spec);
 
-  // AstIf
+  spec = "AstIf";
   EXPECT_EQ( "if(seq(x) seq(1))", AstIf(
       new AstSeq(new AstSymbol(new string("x"))),
-      new AstSeq(new AstNumber(1))).toStr() );
+      new AstSeq(new AstNumber(1))).toStr() ) << amendSpec(spec);
   EXPECT_EQ( "if(seq(x) seq(1) seq(0))", AstIf(
       new AstSeq(new AstSymbol(new string("x"))),
       new AstSeq(new AstNumber(1)),
-      new AstSeq(new AstNumber(0))).toStr() );
+      new AstSeq(new AstNumber(0))).toStr() ) << amendSpec(spec);
 
-  // AstDataDecl
-  EXPECT_EQ( "decldata(foo)", AstDataDecl("foo").toStr() );
-  EXPECT_EQ( "decldata(foo mut)", AstDataDecl("foo",AstDataDecl::eAlloca).toStr() );
+  spec = "AstDataDecl";
+  EXPECT_EQ( "decldata(foo)", AstDataDecl("foo").toStr() ) << amendSpec(spec);
+  EXPECT_EQ( "decldata(foo mut)", AstDataDecl("foo",AstDataDecl::eAlloca).toStr() ) << amendSpec(spec);
   
-  // AstDataDef
-  EXPECT_EQ( "data(decldata(foo))", AstDataDef(new AstDataDecl("foo")).toStr() );
+  spec = "AstDataDef";
+  EXPECT_EQ( "data(decldata(foo))", AstDataDef(new AstDataDecl("foo")).toStr() ) << amendSpec(spec);
   EXPECT_EQ( "data(decldata(foo mut) seq(42))", AstDataDef(
       new AstDataDecl("foo",AstDataDecl::eAlloca),
-      new AstSeq(new AstNumber(42))).toStr() );
+      new AstSeq(new AstNumber(42))).toStr() ) << amendSpec(spec);
   
-  // AstCtList
-  EXPECT_EQ( "", AstCtList().toStr() );
-  EXPECT_EQ( "42", AstCtList(new AstNumber(42)).toStr() );
-  EXPECT_EQ( "42 77", AstCtList(new AstNumber(42),new AstNumber(77)).toStr() );
+  spec = "AstCtList";
+  EXPECT_EQ( "", AstCtList().toStr() ) << amendSpec(spec);
+  EXPECT_EQ( "42", AstCtList(new AstNumber(42)).toStr() ) << amendSpec(spec);
+  EXPECT_EQ( "42 77", AstCtList(new AstNumber(42),new AstNumber(77)).toStr() ) << amendSpec(spec);
 
-  // AstFunDecl
-  EXPECT_EQ( "declfun(foo ())", AstFunDecl("foo").toStr() );
-  EXPECT_EQ( "declfun(foo ())", AstFunDecl("foo", new list<string>()).toStr() );
+  spec = "AstFunDecl";
+  EXPECT_EQ( "declfun(foo ())", AstFunDecl("foo").toStr()) << amendSpec(spec);
+  EXPECT_EQ( "declfun(foo ())", AstFunDecl("foo", new list<string>()).toStr()) << amendSpec(spec);
   list<string>* args = new list<string>();
   args->push_back("arg1");
-  EXPECT_EQ( "declfun(foo (arg1))", AstFunDecl("foo", args).toStr() );
+  EXPECT_EQ( "declfun(foo (arg1))", AstFunDecl("foo", args).toStr()) << amendSpec(spec);
   args = new list<string>();
   args->push_back("arg1");
   args->push_back("arg2");
-  EXPECT_EQ( "declfun(foo (arg1 arg2))", AstFunDecl("foo", args).toStr() );
+  EXPECT_EQ( "declfun(foo (arg1 arg2))", AstFunDecl("foo", args).toStr()) << amendSpec(spec);
 
-  // AstFunDef
-  EXPECT_EQ( "fun(declfun(foo ()) seq())", AstFunDef(new AstFunDecl("foo"),new AstSeq()).toStr() );
+  spec = "AstFunDef";
+  EXPECT_EQ( "fun(declfun(foo ()) seq())", AstFunDef(new AstFunDecl("foo"),new AstSeq()).toStr() ) << amendSpec(spec);
 
-  // AstFunCall
-  EXPECT_EQ( "foo()", AstFunCall("foo", new AstCtList()).toStr() );
+  spec = "AstFunCall";
+  EXPECT_EQ( "foo()", AstFunCall("foo", new AstCtList()).toStr() ) << amendSpec(spec);
   EXPECT_EQ( "foo(42 77)", AstFunCall("foo",
-      new AstCtList(new AstNumber(42), new AstNumber(77))).toStr() );
+      new AstCtList(new AstNumber(42), new AstNumber(77))).toStr() ) << amendSpec(spec);
 }
