9d811c4f9ec3e89ccfaae0344bd854de0f14dd7d
sensorflo <sensorflo@gmail.com>
2014-05-15 16:42:10 +0200

    commit fix for 'main now compiles the file given as command line argument and JIT executes it'

--- src/efc/efc.cpp
+++ src/efc/efc.cpp
@@ -2,20 +2,27 @@
 #include "driver.h"
 #include "ast.h"
 #include <iostream>
+#include <stdexcept>
 using namespace std;
 
 int main(int argc, char** argv) {
   if (argc!=2) {
-    cerr << "Only exactly one argument, the EF program, is allowed.";
+    cerr << "Only exactly one argument, the EF program file name, is allowed.";
     exit(1);
   }
-  IrBuilderAst::staticOneTimeInit();
-  Driver driver(argv[1]);
-  AstSeq* ast = NULL;
-  if (driver.parse(ast)) {
+  try {
+    IrBuilderAst::staticOneTimeInit();
+    Driver driver(argv[1]);
+    AstSeq* ast = NULL;
+    if (driver.parse(ast)) {
+      exit(1);
+    }
+    IrBuilderAst irBuilderAst;
+    cout << irBuilderAst.buildAndRunModule(*ast) << "\n";
+  }
+  catch (const exception& e) {
+    cerr << e.what() << endl;
     exit(1);
   }
-  IrBuilderAst irBuilderAst;
-  cout << irBuilderAst.buildAndRunModule(*ast) << "\n";
   return 0;
 }
