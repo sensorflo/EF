c129bfb08e4b11e56a5ca365d467fa6743da1d5e
sensorflo <sensorflo@gmail.com>
2014-05-23 10:09:00 +0200

    test: added MAKE_DISABLED_TEST_NAME family of macros

--- src/efc/test/test.h
+++ src/efc/test/test.h
@@ -16,6 +16,14 @@ out/specs.html using the tools testdox and asciidoc.*/
 #define MAKE_TEST_NAME2(clause1, clause2) clause1 ## __ ## clause2 
 #define MAKE_TEST_NAME1(clause1) clause1
 
+#define MAKE_DISABLED_TEST_NAME4(given, when, then, sothat_or_because) \
+  DISABLED_ ## given ## __ ## when ## __ ## then ## __ ## sothat_or_because
+#define MAKE_DISABLED_TEST_NAME3(clause1, clause2, clause3)  \
+  DISABLED_ ## clause1 ## __ ## clause2 ## __ ## clause3
+#define MAKE_DISABLED_TEST_NAME(clause1, clause2, clause3) MAKE_DISABLED_TEST_NAME3(clause1, clause2, clause3)
+#define MAKE_DISABLED_TEST_NAME2(clause1, clause2) DISABLED_ ## clause1 ## __ ## clause2 
+#define MAKE_DISABLED_TEST_NAME1(clause1) DISABLED_ ## clause1
+
 // The ENV_ASSERT family of macros assert that the test environment is as
 // expected. It is technically equivalent to the ASSERT family of macros. The
 // difference is that the semantic of ASSERT is to be in the verify section of
