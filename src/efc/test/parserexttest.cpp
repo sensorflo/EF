83e4c8421675350de1d88d3cd47f0e245f81737e
sensorflo <sensorflo@gmail.com>
2015-03-12 18:41:35 +0100

    parserexttest: removed test which already exists in semanticanalizertest

--- src/efc/test/parserexttest.cpp
+++ src/efc/test/parserexttest.cpp
@@ -1,25 +1,5 @@
 #include "test.h"
 #include "../parserext.h"
-#include "../env.h"
-#include "../ast.h"
-#include "../errorhandler.h"
-#include "../objtype.h"
 using namespace testing;
 using namespace std;
-
-TEST(ParserExtTest, MAKE_TEST_NAME(
-    an_already_declared_function_named_foo,
-    mkFunDecl_WITH_name_foo_AND_different_signature,
-    throws) ) {
-  // setup
-  Env env;
-  ErrorHandler errorHandler;
-  ParserExt UUT(env, errorHandler);
-  UUT.mkFunDecl("foo");
-
-  // exercise & verify
-  EXPECT_ANY_THROW(
-    UUT.mkFunDecl("foo",
-      new AstArgDecl("arg1", new ObjTypeFunda(ObjTypeFunda::eInt))));
-}
 
