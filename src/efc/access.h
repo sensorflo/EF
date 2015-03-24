cee4304b5a407de0ede95b9c8a6abe281cc4e4c0
sensorflo <sensorflo@gmail.com>
2015-03-24 07:52:10 +0100

    added printer for the Access enum

--- src/efc/access.h
+++ src/efc/access.h
@@ -1,6 +1,20 @@
 #ifndef ACCESS_H
 #define ACCESS_H
 
+#include <iostream>
+#include <cassert>
+
 enum Access { eRead, eWrite, eIgnore };
+
+inline std::basic_ostream<char>& operator<<(std::basic_ostream<char>& os,
+  Access access) {
+  switch (access) {
+  case eRead: return os << "eRead";
+  case eWrite: return os << "eWrite";
+  case eIgnore: return os << "eIgnore";
+  default: assert(false); return os;
+  }
+}
+
 
 #endif
