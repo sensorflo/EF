519a23605944d566171cfaf56370ea1ec75918c4
sensorflo <sensorflo@gmail.com>
2015-05-01 16:53:06 +0200

    Added ParserApExt; includes dubious Makefile changes

--- src/efc/test/efctest.cpp
+++ src/efc/test/efctest.cpp
@@ -1,10 +1,12 @@
 #include "test.h"
 #include "gtestprinter.h"
 #include "../irgen.h"
+#include "../parserapiext.h"
 using namespace testing;
 
 int main(int argc, char** argv) {
   IrGen::staticOneTimeInit();
+  ParserApiExt::initTokenNames();
   InitGoogleTest(&argc, argv);
   TestEventListeners& listeners = ::UnitTest::GetInstance()->listeners();
   delete listeners.Release(listeners.default_result_printer());
