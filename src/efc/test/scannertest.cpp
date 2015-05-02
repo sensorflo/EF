15928f05188b631dbbc120bc3c28783d58eda8fd
sensorflo <sensorflo@gmail.com>
2015-05-02 15:17:25 +0200

    scannertest: refactoring: extracted testing for an expected sequence of tokens into a new method

--- src/efc/test/scannertest.cpp
+++ src/efc/test/scannertest.cpp
@@ -1,49 +1,72 @@
 #include "test.h"
 #include "../scanner.h"
 #include "driverontmpfile.h"
+#include <string>
+#include <vector>
 using namespace testing;
 using namespace std;
 using namespace yy;
 
+/** Tests that the scanner produces from the given input the given expected
+sequence of tokens. TOK_END_OF_FILE is implicitely appended to the given
+expected token sequence for the caller's convenience. */
+void testScanner(const string& input,
+  const string& spec,
+  vector<Parser::token_type>&& expectedTokenSequence) {
+  // setup
+  DriverOnTmpFile driver( input );
+  Scanner& UUT = driver.scanner(); 
+  expectedTokenSequence.push_back(Parser::token::TOK_END_OF_FILE);
+  
+  for (const auto& expectedToken : expectedTokenSequence) {
+    // execute
+    const auto& actualToken = UUT.pop().token();
+
+    // verify 
+    EXPECT_EQ(expectedToken, actualToken) << amendSpec(spec)
+      << "Input: '" << input << "'\n";
+    EXPECT_FALSE(driver.d().gotError()) << amendSpec(spec)
+      << "Input: '" << input << "'\n";
+    if (Parser::token::TOK_END_OF_FILE==actualToken ||
+      Parser::token::TOK_END_OF_FILE==expectedToken) {
+      break;
+    }
+  }
+}
+
+/** Analogous to testScanner, see there */
+#define TEST_SCANNER(input, spec, ... )                              \
+  {                                                                  \
+    SCOPED_TRACE("testScanner called from here (via TEST_SCANNER)"); \
+    testScanner(input, spec, {__VA_ARGS__} );                        \
+  }
+
 TEST(ScannerTest, MAKE_TEST_NAME(
     an_empty_file,
     yylex,
-    returns_TOK_END_OF_FILE_AND_succeeds) ) {
-  DriverOnTmpFile driver( "" );
-  Scanner& UUT = driver.scanner(); 
-  EXPECT_EQ(Parser::token::TOK_END_OF_FILE, UUT.pop().token() );
-  EXPECT_FALSE( driver.d().gotError() );
+    returns_TOK_END_OF_FILE_AND_succeeds)) {
+  TEST_SCANNER( "", "" );
 }
 
 TEST(ScannerTest, MAKE_TEST_NAME(
     a_keyword,
     yylex,
     returns_token_of_that_keyword_notably_opposed_to_token_for_id_AND_succeeds)) {
-  DriverOnTmpFile driver( "if" );
-  Scanner& UUT = driver.scanner(); 
-  EXPECT_EQ(Parser::token::TOK_IF, UUT.pop().token() );
-  EXPECT_FALSE( driver.d().gotError() );
+  TEST_SCANNER( "if", "", Parser::token::TOK_IF);
 }
 
 TEST(ScannerTest, MAKE_TEST_NAME(
     an_id_consisting_of_concatenated_keywords,
     yylex,
     returns_TOK_ID_AND_succeeds)) {
-  DriverOnTmpFile driver( "ifelse" );
-  Scanner& UUT = driver.scanner(); 
-  EXPECT_EQ(Parser::token::TOK_ID, UUT.pop().token() );
-  EXPECT_FALSE( driver.d().gotError() );
+  TEST_SCANNER( "ifelse", "", Parser::token::TOK_ID);
 }
 
 TEST(ScannerTest, MAKE_TEST_NAME(
     keywords_separated_by_blanks,
     yylex_is_called_repeatedly,
     returns_the_keywords_tokens_AND_succeeds)) {
-  DriverOnTmpFile driver( "if else" );
-  Scanner& UUT = driver.scanner(); 
-  EXPECT_EQ(Parser::token::TOK_IF, UUT.pop().token() );
-  EXPECT_EQ(Parser::token::TOK_ELSE, UUT.pop().token() );
-  EXPECT_FALSE( driver.d().gotError() );
+  TEST_SCANNER( "if else", "", Parser::token::TOK_IF, Parser::token::TOK_ELSE);
 }
 
 TEST(ScannerTest, MAKE_TEST_NAME(
@@ -167,11 +190,7 @@ TEST(ScannerTest, MAKE_TEST_NAME(
     a_colon_followed_by_an_equal_without_blanks_inbetween,
     yylex_is_called,
     returns_the_single_token_COLON_EQUAL_opposed_to_the_two_separate_tokens_COLON_and_EQUAL)) {
-  DriverOnTmpFile driver( ":=" );
-  Scanner& UUT = driver.scanner(); 
-  EXPECT_EQ(Parser::token::TOK_COLON_EQUAL, UUT.pop().token() );
-  EXPECT_EQ(Parser::token::TOK_END_OF_FILE, UUT.pop().token() );
-  EXPECT_FALSE( driver.d().gotError() );
+  TEST_SCANNER( ":=", "", Parser::token::TOK_COLON_EQUAL);
 }
 
 TEST(ScannerTest, MAKE_TEST_NAME(
@@ -217,12 +236,7 @@ TEST(ScannerTest, MAKE_TEST_NAME(
     a_colon_followed_by_blanks_followed_by_an_equal,
     yylex_is_called_repeatedly,
     returns_two_separate_tokens_COLON_and_EQUAL_opposed_to_the_single_token_COLON_EQUAL)) {
-  DriverOnTmpFile driver( ": =" );
-  Scanner& UUT = driver.scanner(); 
-  EXPECT_EQ(Parser::token::TOK_COLON, UUT.pop().token() );
-  EXPECT_EQ(Parser::token::TOK_EQUAL, UUT.pop().token() );
-  EXPECT_EQ(Parser::token::TOK_END_OF_FILE, UUT.pop().token() );
-  EXPECT_FALSE( driver.d().gotError() );
+  TEST_SCANNER( ": =", "", Parser::token::TOK_COLON, Parser::token::TOK_EQUAL);
 }
 
 TEST(ScannerTest, MAKE_TEST_NAME(
@@ -231,51 +245,22 @@ TEST(ScannerTest, MAKE_TEST_NAME(
     returns_a_token_sequence_which_ignores_the_comment)) {
 
   string spec = "Trivial example";
-  {
-    DriverOnTmpFile driver( "//foo\n" );
-    Scanner& UUT = driver.scanner(); 
-    EXPECT_EQ(Parser::token::TOK_END_OF_FILE, UUT.pop().token() ) << amendSpec(spec);
-    EXPECT_FALSE( driver.d().gotError() ) << amendSpec(spec);
-  }
+  TEST_SCANNER( "//foo\n", spec );
 
   spec = "Simple example";
-  {
-    DriverOnTmpFile driver( "if //foo\n if" );
-    Scanner& UUT = driver.scanner(); 
-    EXPECT_EQ(Parser::token::TOK_IF, UUT.pop().token() ) << amendSpec(spec);
-    EXPECT_EQ(Parser::token::TOK_IF, UUT.pop().token() ) << amendSpec(spec);
-    EXPECT_EQ(Parser::token::TOK_END_OF_FILE, UUT.pop().token() ) << amendSpec(spec);
-    EXPECT_FALSE( driver.d().gotError() ) << amendSpec(spec);
-  }
+  TEST_SCANNER( "if //foo\n if", spec,
+    Parser::token::TOK_IF, Parser::token::TOK_IF );
 
   spec = "Border case example: // are the last two chars before the newline";
-  {
-    DriverOnTmpFile driver( "if //\n if" );
-    Scanner& UUT = driver.scanner(); 
-    EXPECT_EQ(Parser::token::TOK_IF, UUT.pop().token() ) << amendSpec(spec);
-    EXPECT_EQ(Parser::token::TOK_IF, UUT.pop().token() ) << amendSpec(spec);
-    EXPECT_EQ(Parser::token::TOK_END_OF_FILE, UUT.pop().token() ) << amendSpec(spec);
-    EXPECT_FALSE( driver.d().gotError() ) << amendSpec(spec);
-  }
+  TEST_SCANNER( "if //\n if", spec,
+    Parser::token::TOK_IF, Parser::token::TOK_IF );
 
   spec = "Border case example: the single line comment is on the last line"
     "of the file which is not delimited by newline";
-  {
-    DriverOnTmpFile driver( "if //foo" );
-    Scanner& UUT = driver.scanner(); 
-    EXPECT_EQ(Parser::token::TOK_IF, UUT.pop().token() ) << amendSpec(spec);
-    EXPECT_EQ(Parser::token::TOK_END_OF_FILE, UUT.pop().token() ) << amendSpec(spec);
-    EXPECT_FALSE( driver.d().gotError() ) << amendSpec(spec);
-  }
+  TEST_SCANNER( "if //foo", spec, Parser::token::TOK_IF);
 
   spec = "Border case example: // are the last two chars in the file";
-  {
-    DriverOnTmpFile driver( "if //" );
-    Scanner& UUT = driver.scanner(); 
-    EXPECT_EQ(Parser::token::TOK_IF, UUT.pop().token() ) << amendSpec(spec);
-    EXPECT_EQ(Parser::token::TOK_END_OF_FILE, UUT.pop().token() ) << amendSpec(spec);
-    EXPECT_FALSE( driver.d().gotError() ) << amendSpec(spec);
-  }
+  TEST_SCANNER( "if //", spec, Parser::token::TOK_IF);
 }
 
 TEST(ScannerTest, MAKE_TEST_NAME(
@@ -284,51 +269,22 @@ TEST(ScannerTest, MAKE_TEST_NAME(
     returns_a_token_sequence_which_ignores_the_comment)) {
 
   string spec = "Trivial example";
-  {
-    DriverOnTmpFile driver( "#!foo\n" );
-    Scanner& UUT = driver.scanner(); 
-    EXPECT_EQ(Parser::token::TOK_END_OF_FILE, UUT.pop().token() ) << amendSpec(spec);
-    EXPECT_FALSE( driver.d().gotError() ) << amendSpec(spec);
-  }
+  TEST_SCANNER( "#!foo\n", spec);
 
   spec = "Simple example";
-  {
-    DriverOnTmpFile driver( "if #!foo\n if" );
-    Scanner& UUT = driver.scanner(); 
-    EXPECT_EQ(Parser::token::TOK_IF, UUT.pop().token() ) << amendSpec(spec);
-    EXPECT_EQ(Parser::token::TOK_IF, UUT.pop().token() ) << amendSpec(spec);
-    EXPECT_EQ(Parser::token::TOK_END_OF_FILE, UUT.pop().token() ) << amendSpec(spec);
-    EXPECT_FALSE( driver.d().gotError() ) << amendSpec(spec);
-  }
+  TEST_SCANNER( "if #!foo\n if", spec,
+    Parser::token::TOK_IF, Parser::token::TOK_IF);
 
   spec = "Border case example: #! are the last two chars before the newline";
-  {
-    DriverOnTmpFile driver( "if #!\n if" );
-    Scanner& UUT = driver.scanner(); 
-    EXPECT_EQ(Parser::token::TOK_IF, UUT.pop().token() ) << amendSpec(spec);
-    EXPECT_EQ(Parser::token::TOK_IF, UUT.pop().token() ) << amendSpec(spec);
-    EXPECT_EQ(Parser::token::TOK_END_OF_FILE, UUT.pop().token() ) << amendSpec(spec);
-    EXPECT_FALSE( driver.d().gotError() ) << amendSpec(spec);
-  }
+  TEST_SCANNER( "if #!\n if", spec,
+    Parser::token::TOK_IF, Parser::token::TOK_IF);
 
   spec = "Border case example: the single line comment is on the last line"
     "of the file which is not delimited by newline";
-  {
-    DriverOnTmpFile driver( "if #!foo" );
-    Scanner& UUT = driver.scanner(); 
-    EXPECT_EQ(Parser::token::TOK_IF, UUT.pop().token() ) << amendSpec(spec);
-    EXPECT_EQ(Parser::token::TOK_END_OF_FILE, UUT.pop().token() ) << amendSpec(spec);
-    EXPECT_FALSE( driver.d().gotError() ) << amendSpec(spec);
-  }
+  TEST_SCANNER( "if #!foo", spec,  Parser::token::TOK_IF);
 
   spec = "Border case example: #! are the last two chars in the file";
-  {
-    DriverOnTmpFile driver( "if #!" );
-    Scanner& UUT = driver.scanner(); 
-    EXPECT_EQ(Parser::token::TOK_IF, UUT.pop().token() ) << amendSpec(spec);
-    EXPECT_EQ(Parser::token::TOK_END_OF_FILE, UUT.pop().token() ) << amendSpec(spec);
-    EXPECT_FALSE( driver.d().gotError() ) << amendSpec(spec);
-  }
+  TEST_SCANNER( "if #!", spec,  Parser::token::TOK_IF);
 }
 
 TEST(ScannerTest, MAKE_TEST_NAME(
@@ -337,64 +293,26 @@ TEST(ScannerTest, MAKE_TEST_NAME(
     returns_a_token_sequence_which_ignores_the_comment)) {
 
   string spec = "trivial example";
-  {
-    DriverOnTmpFile driver( "/*foo*/" );
-    Scanner& UUT = driver.scanner(); 
-    EXPECT_EQ(Parser::token::TOK_END_OF_FILE, UUT.pop().token() );
-    EXPECT_FALSE( driver.d().gotError() );
-  }
+  TEST_SCANNER( "/*foo*/", spec);
 
   spec = "simple example";
-  {
-    DriverOnTmpFile driver( "x /*foo*/ y" );
-    Scanner& UUT = driver.scanner(); 
-    EXPECT_EQ(Parser::token::TOK_ID, UUT.pop().token() );
-    EXPECT_EQ(Parser::token::TOK_ID, UUT.pop().token() );
-    EXPECT_EQ(Parser::token::TOK_END_OF_FILE, UUT.pop().token() );
-    EXPECT_FALSE( driver.d().gotError() );
-  }
+  TEST_SCANNER( "x /*foo*/ y", spec,
+    Parser::token::TOK_ID, Parser::token::TOK_ID);
 
   spec = "example: comment contains newlines";
-  {
-    DriverOnTmpFile driver( "x /*foo\nbar*/ y" );
-    Scanner& UUT = driver.scanner(); 
-    EXPECT_EQ(Parser::token::TOK_ID, UUT.pop().token() );
-    EXPECT_EQ(Parser::token::TOK_ID, UUT.pop().token() );
-    EXPECT_EQ(Parser::token::TOK_END_OF_FILE, UUT.pop().token() );
-    EXPECT_FALSE( driver.d().gotError() );
-  }
+  TEST_SCANNER( "x /*foo\nbar*/ y", spec,
+    Parser::token::TOK_ID, Parser::token::TOK_ID);
 
   spec = "example: comment contains *";
-  {
-    DriverOnTmpFile driver( "x /* foo*bar */ y" );
-    Scanner& UUT = driver.scanner(); 
-    EXPECT_EQ(Parser::token::TOK_ID, UUT.pop().token() );
-    EXPECT_EQ(Parser::token::TOK_ID, UUT.pop().token() );
-    EXPECT_EQ(Parser::token::TOK_END_OF_FILE, UUT.pop().token() );
-    EXPECT_FALSE( driver.d().gotError() );
-  }
+  TEST_SCANNER( "x /* foo*bar */ y", spec,
+    Parser::token::TOK_ID, Parser::token::TOK_ID);
 
   spec = "border case example: empty comment";
-  {
-    DriverOnTmpFile driver( "/**/" );
-    Scanner& UUT = driver.scanner(); 
-    EXPECT_EQ(Parser::token::TOK_END_OF_FILE, UUT.pop().token() );
-    EXPECT_FALSE( driver.d().gotError() );
-  }
+  TEST_SCANNER( "/**/", spec);
 
   spec = "border case example: only stars inside comment";
-  {
-    DriverOnTmpFile driver( "/***/" );
-    Scanner& UUT = driver.scanner(); 
-    EXPECT_EQ(Parser::token::TOK_END_OF_FILE, UUT.pop().token() );
-    EXPECT_FALSE( driver.d().gotError() );
-  }
+  TEST_SCANNER( "/***/", spec);
 
   spec = "border case example: stars inside comment";
-  {
-    DriverOnTmpFile driver( "/* foo**bar */" );
-    Scanner& UUT = driver.scanner(); 
-    EXPECT_EQ(Parser::token::TOK_END_OF_FILE, UUT.pop().token() );
-    EXPECT_FALSE( driver.d().gotError() );
-  }
+  TEST_SCANNER( "/* foo**bar */", spec);
 }
